796d5fa5e456e71da34e347e1132ac7c8b6a157e
karisyg <kariukipeter30@gmail.com>
2017-11-02 15:20:51 +0300

    update js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
 $(document).ready(function(){
 	var btnclick = $("#answer");
 	//var userinput = $("input#userinput").val();
-	var userinput = document.getElementById("userinput").value;
+	//var userinput = document.getElementById("userinput").value;
 	var pwarning = $("#warning");
 	var welcomeheader =$("#welcomeheader");
 	$("#userinput").click(function() {
@@ -39,5 +39,27 @@ $(document).ready(function(){
 		// 	pwarning.text("Hey, mehn ! add your Username! Here");
 		// }
 	});
+	$("#btnalert").click(function() {
+		console.log($("#btninput").val());
+	});
 
 });
+
+
+
+
+var myFlower = {};
+myFlower.height=10;
+
+console.log(myFlower);
+
+
+// function grow(value) {
+// 	return myFlower['height']+value;
+// }
+
+var grow =(value)=>{
+	return myFlower.height+value;
+}
+
+console.log(grow(23));
