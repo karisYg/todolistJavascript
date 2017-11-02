5f202fd80bc72dcf1985e695af98aaa68b4de396
karisyg <kariukipeter30@gmail.com>
2017-11-02 13:18:54 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,29 @@
 // }
 
 // giveme();
+
+$(document).ready(function(){
+	var btnclick = $("#answer");
+	var username = $("#userinput").val();
+	var pwarning = $("#warning");
+	$("#userinput").click(function(event) {
+		pwarning.text("");
+		$(this).css({
+			borderColor : '#000',
+			color: '#000'
+		});
+
+	});
+	btnclick.click(function(event) {
+		if (username!="") {
+
+		}else{
+			$("#userinput").css({
+				borderColor: ' #ea4d4d',
+				color: 'red'
+			});
+			pwarning.text("Hey, mehn ! add your Username! Here");
+		}
+	});
+
+});
