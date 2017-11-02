dc637d7882de254c6aacc0fed82d5f72bf2d2cf8
karisyg <kariukipeter30@gmail.com>
2017-11-02 12:06:59 +0300

    file uploads

---
+++ README.md
@@ -0,0 +1,5 @@
+# Todo List Application
+
+# Description
+### this simple application is supposed to make me conversant with constructors, prototype 
+### the application should also make me understand everything about localStorage
---
+++ js/script.js
@@ -0,0 +1,12 @@
+var giveme = ()=>{
+  var username=prompt("What is your Name ");
+  if (username!="") {
+  	username="<strong> NONAME </strong>";
+  }
+  var salutations ="Hae, " + username + " welcome to Todolist App";
+  document.getElementById("name").innerHTML=salutations;
+
+
+}
+
+giveme();
